091152b3ce2471efe0bcfe7cf18fdc4631524e9a
TalhaSyed65737 <talhauamr397@gmail.com>
2024-09-05 23:07:11 -0700

    update

--- 2.js
+++ 2.js
@@ -5,9 +5,9 @@
 
 
 
-// for(i=0; i<=10; i++){
-//     console.log(i + " x" + " 2" + " =" + i * 2);
-// }
+for(i=0; i<=10; i++){
+    console.log(i + " x" + " 2" + " =" + i * 2);
+}
 
 // for(i=10; i>=0; i--){
 //     console.log(i + " x" + " 2" + " =" + i * 2);
